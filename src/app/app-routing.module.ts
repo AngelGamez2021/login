7dcc25c1250a1b75bf9558d1b73b095ede51bd75
AngelGamez2021 <agamez6@unisimon.edu.co>
2022-04-25 17:27:51 -0500

    avance25DeAbril

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './components/cards/cards.component';
+import { LoginComponent } from './components/login/login.component';
+import { ModalEditUserComponent } from './components/modal-edit-user/modal-edit-user.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { UserComponent } from './components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signUp', component: SignUpComponent},
+  {path: 'cardList', component: CardsComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'editUser', component: ModalEditUserComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,34 +9,76 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class LoginComponent implements OnInit {
 
-  loginForm!: FormGroup;
+  formLogin!: FormGroup;
   submitted = false;
 
-  constructor(private fb: FormBuilder) {
+  messageError = {
 
-    this.loginForm = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-    });
+    email: {
+      required: 'Requerid Email',
+      pattern: 'Email invalid',
+    },
+    password: {
+      required: 'Password requerid',
+      minLength: 'Password Invalid',
+    },
 
+  };
 
-  }
+  constructor(
+    // public activeModal: NgbActiveModal,
+    // private modalService: NgbModal,
+    // private store: Store,
+    private fb: FormBuilder
+  ) {}
+
   ngOnInit(): void {
+    this.formLogin = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern(/^[A-z]+[a-zA-Z0-9._]+@[a-z]+\.[a-z.]{2,6}$/),],],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+    this.loginUser();
   }
 
   loginUser() {
-    this.submitted = true;
 
-    if (this.loginForm.invalid) {
-      return;
-      // me retorna al estado inicial que viene siendo invalido. mientras no se llenen todos los datos
+    if( this.submitted = true){
+      // this.loginUser.controls.password.diable();
+      const form = {
+        email: this.formLogin.value.email,
+        password: this.formLogin.value.password,
+      };
+
+      this.formLogin.patchValue(form);
+      console.log(form);
+      
     }
-
-    const empleado: any = {
-      email: this.loginForm.value.email,
-      password: this.loginForm.value.password,
-    }
-
   }
 
+
+  // onSubmit() {
+  //   if (!this.formLogin.invalid) {
+  //     if (this.submitted === true) {
+  //       console.log('Registrado con exito');
+        
+  //     } else {
+
+  //       console.log('Registrado con exito');
+        
+
+  //     }
+  //   }
+  // }
+
+ getError(nameError: string): string {
+    // if (!this.formLogin.get(nameError).touched) {
+       return '';
+    // }
+    // const errors = this.formLogin.get(nameError).errors || {};
+    // const error = Object.keys(errors)[0];
+
+    // return this.messageError[nameError][error];
+   }
 }
+
+
